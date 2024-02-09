ac615223s5/logseq

commit a3e64aa5a34d89504bf084aa6593bd1730d7a1db

feat: /scheduled and /deadline for db graphs

src/main/frontend/components/property.cljs
@@ -441,8 +441,10 @@
 
 (rum/defcs new-property < rum/reactive
   (rum/local false ::new-property?)
-  (rum/local nil ::property-key)
   (rum/local nil ::property-value)
+  {:will-unmount (fn [state]
+                   (state/set-state! :editor/new-property-key nil)
+                   state)}
   [state block id keyboard-triggered? opts]
   (let [*new-property? (::new-property? state)
         container-id (state/sub :editor/properties-container)
@@ -450,7 +452,7 @@
 
     (when-not (and (:in-block-container? opts) (not keyboard-triggered?))
       [:div.ls-new-property
-       (let [*property-key (::property-key state)
+       (let [*property-key (:editor/new-property-key @state/state)
              *property-value (::property-value state)]
          (cond
            new-property?

deps/db/src/logseq/db/frontend/property.cljs
@@ -65,6 +65,12 @@
               (mapv #(hash-map :value % :uuid (random-uuid))
                     ["Urgent" "High" "Medium" "Low"])
               :visible true}
+   :scheduled {:original-name "Scheduled"
+               :schema {:type :date}
+               :visible true}
+   :deadline {:original-name "Deadline"
+              :schema {:type :date}
+              :visible true}
 
    ;; TODO: Add more props :Assignee, :Estimate, :Cycle, :Project
 

src/main/frontend/commands.cljs
@@ -308,9 +308,9 @@
     ;; task management
       (get-statuses)
       [["Deadline" [[:editor/clear-current-slash]
-                    [:editor/show-date-picker :deadline]]]
+                    [:editor/set-deadline]]]
        ["Scheduled" [[:editor/clear-current-slash]
-                     [:editor/show-date-picker :scheduled]]]]
+                     [:editor/set-scheduled]]]]
 
     ;; priority
       (get-priorities)
@@ -675,6 +675,16 @@
   (if (config/db-based-graph? (state/get-current-repo))
     (db-based-set-priority priority)
     (file-based-set-priority priority)))
+
+(defmethod handle-step :editor/set-scheduled [[_]]
+  (if (config/db-based-graph? (state/get-current-repo))
+    (state/pub-event! [:editor/new-property "Scheduled"])
+    (handle-step :editor/show-date-picker :scheduled)))
+
+(defmethod handle-step :editor/set-deadline [[_]]
+  (if (config/db-based-graph? (state/get-current-repo))
+    (state/pub-event! [:editor/new-property "Deadline"])
+    (handle-step :editor/show-date-picker :deadline)))
 
 (defmethod handle-step :editor/insert-properties [[_ _] _format]
   (when-let [input-id (state/get-edit-input-id)]

src/main/frontend/handler/events.cljs
@@ -916,7 +916,7 @@
   (when-let [blocks (and block (db-model/get-block-immediate-children (state/get-current-repo) (:block/uuid block)))]
     (editor-handler/toggle-blocks-as-own-order-list! blocks)))
 
-(defmethod handle :editor/new-property [[_]]
+(defmethod handle :editor/new-property [[_ property-key]]
   (p/do!
     (when-let [edit-block (state/get-edit-block)]
      (when-let [block-id (:block/uuid edit-block)]
@@ -926,6 +926,8 @@
          (when collapsed?
            (editor-handler/set-blocks-collapsed! [block-id] false)))))
     (editor-handler/save-current-block!)
+    (when property-key
+      (state/set-state! :editor/new-property-key property-key))
     (property-handler/editing-new-property!)))
 
 (rum/defc multi-tabs-dialog

src/main/frontend/state.cljs
@@ -131,6 +131,7 @@
       :editor/content                        (atom {})
       :editor/block                          (atom nil)
       :editor/new-property-input-id          (atom nil)
+      :editor/new-property-key               (atom nil)
       :editor/properties-container           (atom nil)
       :editor/block-dom-id                   (atom nil)
       :editor/set-timestamp-block            (atom nil) ;; click rendered block timestamp-cp to set timestamp

commit 9d7a04e1a5d445d26fdf31eb396d328954e00c4b

fix: not able to add page or block property
fixes LOG-3052. Caused by a3e64aa5a34d89504bf084aa6593bd1730d7a1db

src/main/frontend/components/property.cljs
@@ -441,6 +441,7 @@
 
 (rum/defcs new-property < rum/reactive
   (rum/local false ::new-property?)
+  (rum/local nil ::property-key)
   (rum/local nil ::property-value)
   {:will-unmount (fn [state]
                    (state/set-state! :editor/new-property-key nil)
@@ -452,7 +453,8 @@
 
     (when-not (and (:in-block-container? opts) (not keyboard-triggered?))
       [:div.ls-new-property
-       (let [*property-key (:editor/new-property-key @state/state)
+       (let [global-property-key (:editor/new-property-key @state/state)
+             *property-key (if @global-property-key global-property-key (::property-key state))
              *property-value (::property-value state)]
          (cond
            new-property?