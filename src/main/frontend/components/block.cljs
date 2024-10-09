ac615223s5/logseq

commit 741a07fdaf0b3f50b284d746b77d1d777d9880c5

fix(ux): sometimes ESC not working for the code block editor

src/main/frontend/components/block.cljs
@@ -2708,16 +2708,19 @@
                      (count (:block/_refs block))
                      (rum/react *refs-count))
         table? (:table? config)
+        editor-block (state/sub :editor/block)
+        raw-mode-block (state/sub :editor/raw-mode-block)
         type-block-editor? (and (contains? #{:code} (:logseq.property.node/display-type block))
-                                (not= (:db/id block) (:db/id (state/sub :editor/raw-mode-block))))
-        config (assoc config :block-parent-id block-id)]
+                                (not= (:db/id block) (:db/id raw-mode-block)))
+        config (assoc config :block-parent-id block-id)
+        editing-local? (or edit? (and editor-block (= (:db/id editor-block) (:db/id block))))]
     [:div.block-content-or-editor-wrap
      {:class (when (:page-title? config) "ls-page-title-container")
       :data-node-type (some-> (:logseq.property.node/display-type block) name)}
      (when (and db-based? (not table?)) (block-positioned-properties config block :block-left))
      [:div.block-content-or-editor-inner
       [:div.flex.flex-1.flex-row.gap-1.items-center
-       (if (and edit? editor-box (not type-block-editor?))
+       (if (and editor-box editing-local? (not type-block-editor?))
          [:div.editor-wrapper.flex.flex-1
           {:id editor-id
            :class (util/classnames [{:opacity-50 (boolean (or (ldb/built-in? block) (ldb/journal? block)))}])}

commit 09aee239f75059d23ba0787930d9fa3bd10ab10a

fix: click a block edit all the same blocks
Broken by 741a07fda.

src/main/frontend/components/block.cljs
@@ -2763,19 +2763,17 @@
                      (count (:block/_refs block))
                      (rum/react *refs-count))
         table? (:table? config)
-        editor-block (state/sub :editor/block)
         raw-mode-block (state/sub :editor/raw-mode-block)
         type-block-editor? (and (contains? #{:code} (:logseq.property.node/display-type block))
                                 (not= (:db/id block) (:db/id raw-mode-block)))
-        config (assoc config :block-parent-id block-id)
-        editing-local? (or edit? (and editor-block (= (:db/id editor-block) (:db/id block))))]
+        config (assoc config :block-parent-id block-id)]
     [:div.block-content-or-editor-wrap
      {:class (when (:page-title? config) "ls-page-title-container")
       :data-node-type (some-> (:logseq.property.node/display-type block) name)}
      (when (and db-based? (not table?)) (block-positioned-properties config block :block-left))
      [:div.block-content-or-editor-inner
       [:div.flex.flex-1.flex-row.gap-1.items-center
-       (if (and editor-box editing-local? (not type-block-editor?))
+       (if (and editor-box edit? (not type-block-editor?))
          [:div.editor-wrapper.flex.flex-1
           {:id editor-id
            :class (util/classnames [{:opacity-50 (boolean (or (ldb/built-in? block) (ldb/journal? block)))}])}
@@ -3441,7 +3439,10 @@
        (rum/with-key
          (block-container-inner state repo config' block
                                 {:navigating-block navigating-block :navigated? navigated?})
-         (str "block-inner" (:block/uuid block)))))))
+         (str "block-inner-"
+              (:container-id config)
+              "-"
+              (:block/uuid block)))))))
 
 (defn divide-lists
   [[f & l]]