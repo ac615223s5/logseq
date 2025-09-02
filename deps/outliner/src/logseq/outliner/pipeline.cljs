ac615223s5/logseq

commit 6f6a3a960d52b784d60f7abf858e39eaf586f2f1

fix: add journal day ref to scheduled and deadline properties only
not all :datetime properties

deps/outliner/src/logseq/outliner/pipeline.cljs
@@ -184,18 +184,12 @@
                                              (and (coll? v) (every? page-or-object? v))
                                              (map :db/id v)
 
+                                             (contains? #{:logseq.property/scheduled :logseq.property/deadline} (:db/ident (d/entity db property)))
+                                             (when-let [journal-day (get-journal-day-from-long db v)]
+                                               [journal-day])
+
                                              :else
-                                             (let [datetime? (= :datetime (:logseq.property/type (d/entity db property)))]
-                                               (cond
-                                                 (and datetime? (coll? v))
-                                                 (keep #(get-journal-day-from-long db %) v)
-
-                                                 datetime?
-                                                 (when-let [journal-day (get-journal-day-from-long db v)]
-                                                   [journal-day])
-
-                                                 :else
-                                                 nil))))))
+                                             nil))))
 
         property-refs (concat property-key-refs property-value-refs)
         content-refs (block-content-refs db block)]

commit 91022f907cc4e04b653a8a7fd0dc1b135f272526

fix(regression): journal refs not being built for user :datetime properties
Regression accidentally introduced in 6f6a3a960d52b784d60f7abf858e39eaf586f2f1

deps/outliner/src/logseq/outliner/pipeline.cljs
@@ -153,9 +153,32 @@
     (let [day (date-time-util/ms->journal-day v)]
       (:e (first (d/datoms db :avet :block/journal-day day))))))
 
-
 (def ^:private private-built-in-props (set (keep (fn [[k v]] (when-not (get-in v [:schema :public?]) k))
                                                  db-property/built-in-properties)))
+
+(defn- build-journal-refs-for-datetime-properties
+  "For a given property pair, builds a coll of journal refs for select built-in
+  :datetime properties and all user :datetime properties. Otherwise returns nil"
+  [db property v]
+  (let [property-ent (d/entity db property)
+        allowed-datetime? (and (= :datetime (:logseq.property/type property-ent))
+                               ;; Only allow a few built-in properties as some built-in properties
+                               ;; like :logseq.property.embedding/hnsw-label-updated-at create undesirable refs
+                               (if (db-property/internal-property? (:db/ident property-ent))
+                                 (contains? #{:logseq.property/scheduled :logseq.property/deadline} (:db/ident property-ent))
+                                 ;; All user properties are allowed to create refs but not plugin properties
+                                 (not (db-property/plugin-property? (:db/ident property-ent)))))]
+    (cond
+      (and allowed-datetime? (coll? v))
+      (keep #(get-journal-day-from-long db %) v)
+
+      allowed-datetime?
+      (when-let [journal-day (get-journal-day-from-long db v)]
+        [journal-day])
+
+      :else
+      nil)))
+
 (defn db-rebuild-block-refs
   "Rebuild block refs for DB graphs"
   [db block]
@@ -185,13 +208,8 @@
                                              (page-or-object? v)
                                              [(:db/id v)]
 
-                                             (contains? #{:logseq.property/scheduled :logseq.property/deadline} (:db/ident (d/entity db property)))
-                                             (when-let [journal-day (get-journal-day-from-long db v)]
-                                               [journal-day])
-
                                              :else
-                                             nil))))
-
+                                             (build-journal-refs-for-datetime-properties db property v)))))
         property-refs (concat property-key-refs property-value-refs)
         content-refs (block-content-refs db block)]
     (->> (concat (map ref->eid (:block/tags block))